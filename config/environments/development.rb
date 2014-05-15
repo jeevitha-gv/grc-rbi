3dc0e6ba1d9602993287e1a1e9c8de2b9c12fcf9
ghayathri <aghayathri@hotmail.com>
2014-05-15 16:51:40 +0530

    #9748-user login and sign-up using devise- added modified files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,16 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+      ActionMailer::Base.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => "nixers@fixrnix.in",
+      :password => "s3cure@fixnix!",
+      :authentication => "plain",
+      :enable_starttls_auto => true
+} 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
