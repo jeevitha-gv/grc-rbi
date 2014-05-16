c7b30d313e018978299817ab8bf1131e2f2fac98
selvavalli <selvavalli@railsfactory.org>
2014-05-16 18:19:52 +0530

    #9774 - header customization in active admin

--- app/admin/role.rb
+++ app/admin/role.rb
@@ -1,8 +1,8 @@
 ActiveAdmin.register Role do
- 
+ menu :if => proc{ current_admin_user.present? }
  #authentication
   controller do
-   before_filter :authenticate_admin_user!
+   #~ before_filter :authenticate_admin_user!
   end
  
   permit_params :title, :company_id
--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register_page "Settings" do
+	
+	menu :if => proc{ !current_admin_user.present? }
 	controller do
       #~ before_filter :authenticate_admin_user!
 	end
