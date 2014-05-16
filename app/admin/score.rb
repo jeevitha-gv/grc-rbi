c5cf58a0153472fe4b7b7f0e9ebc5e2f1a000f7b
selvavalli <selvavalli@railsfactory.org>
2014-05-16 10:48:44 +0530

    #9761-active admin for score index page customization

--- app/admin/score.rb
+++ app/admin/score.rb
@@ -1,17 +1,24 @@
 ActiveAdmin.register Score do
 
+    # remove new record creation option
+    actions :all, :except => [:new]
+
+  permit_params :id, :level, :description
   
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
+  #display the required fields in index
+  index do                            
+    column :level        
+    column :description     
+    actions    
+  end     
+  
+  #form for edit scores
+   form do |f|
+    f.inputs "Edit scores" do
+      f.input :level
+      f.input :description
+    end
+    f.actions
+  end
   
 end
