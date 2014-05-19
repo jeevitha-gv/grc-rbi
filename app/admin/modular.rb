a10c8d14f61a32c8d24ad1321c257aad910869a8
Madhusudhan <madhusudhan@railsfactory.org>
2014-05-19 11:55:35 +0530

    #9749 added modular part to admin with association

---
+++ app/admin/modular.rb
@@ -0,0 +1,12 @@
+ActiveAdmin.register Modular do
+
+  permit_params :controller_name, :action_name, :section_id
+
+  index do
+    selectable_column
+    id_column
+    column :controller_name
+    column :action_name
+    actions
+  end
+end
