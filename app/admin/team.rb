d4ce6686e256be6b9762ca315d5f8e65f8b17a35
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-20 17:20:09 +0530

    # 9775 team creation

--- app/admin/team.rb
+++ app/admin/team.rb
@@ -2,6 +2,8 @@ ActiveAdmin.register Team do
 
       menu :if => proc{ !current_admin_user.present? }
 
+      permit_params :name
+
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
