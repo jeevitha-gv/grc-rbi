0b4c52bcbea826fd7c40aee3f14c3c33363f8504
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-05-21 14:59:41 +0530

    #9747 - Company CRUD - Routes root changed to SaaS Signup

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'audits#index'
+   root 'companies#new'
   
   resources :companies do
     member do
