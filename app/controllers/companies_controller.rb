2cbdb5c9de8ad0f9c5d3a5fc0538bc5fe51ee56a
Madhusudhan <madhusudhan@railsfactory.org>
2014-05-21 21:13:09 +0530

    changed to attachment file

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -27,6 +27,6 @@ class CompaniesController < ApplicationController
 
   private
   def company_params
-    params.require(:company).permit(:name, :primary_email, :secondary_email, :domain, :address1, :address2, :country_id, :contact_no, :timezone, :attachments_attributes =>[:file])
+    params.require(:company).permit(:name, :primary_email, :secondary_email, :domain, :address1, :address2, :country_id, :contact_no, :timezone, :attachments_attributes =>[:attachment_file])
   end
 end
