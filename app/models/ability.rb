f6e68a87c4c14c8e54099a8c5f83926408011649
selvavalli <selvavalli@railsfactory.org>
2014-05-22 13:57:30 +0530

    condition added to check user role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,13 @@ class Ability
         can :manage, :all
       else
         user_previleges =  user.user_previleges
+        unless user_previleges.empty?
           user_previleges.each do |action_name, model|
             model.each do |modular|
               can modular.action_name.to_sym, modular.model_name.constantize
             end
           end
+        end
       end
     end
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,14 @@ class User < ActiveRecord::Base
 
   delegate :title, to: :dealer, prefix: true
 
+  def is?( requested_role)
+    self.role == requested_role.to_s if self.role.present?
+  end
+  
+  def user_previleges
+    @grouped_modular_action ||= self.role.previleges.map(&:modular).group_by(&:action_name) if self.role.present?
+  end
+  
   protected
 
   def password_required?
