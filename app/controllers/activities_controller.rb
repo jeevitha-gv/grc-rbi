64842bd4a7ba7d08a915cd96bc070fa35f164c66
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-05-23 17:55:15 +0530

    Activity Log Parsing and Displaying Done

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,16 @@
 class ActivitiesController < ApplicationController
+
+  helper_method :parse_and_convert
  
   def index
    @activities = PublicActivity::Activity.all
   end
+
+  def parse_and_convert(text)
+  	thing = text.slice(0..(text.index('.')-1))
+  	action = text.slice((text.index('.')+1)..(text.length)) + 'd'
+  	activity = action + " a " + thing + " from "
+    return activity
+  end
+
 end
