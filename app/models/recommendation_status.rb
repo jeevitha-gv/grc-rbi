65ff7107d33c70d6b0f6698c41ffa4d4e6e682ac
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-23 22:43:59 +0530

    #9829 PublicActivity seed table added

--- app/models/recommendation_status.rb
+++ app/models/recommendation_status.rb
@@ -1,5 +1,5 @@
 class RecommendationStatus < ActiveRecord::Base
-    publicactivity gem
+    #publicactivity gem
    include PublicActivity::Model
    tracked owner: ->(controller, model) { controller && controller.current_user }
    tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
