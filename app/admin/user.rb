c880afa374a855b95fc0e49538520fefb19a1b78
ghayathri <aghayathri@hotmail.com>
2014-05-23 18:18:53 +0530

    Update the profile info of user in active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -20,24 +20,40 @@ show do
   attributes_table :full_name, :email, :user_name
 end
 
-  permit_params :full_name, :email, :user_name, :is_disabled, profile_attributes: [:personal_email, :address2, :address1]
+  permit_params :full_name, :email, :user_name, :password , :password_confirmation, :is_disabled, profile_attributes: [:personal_email, :address2, :address1]
 
 
 form do |f|
-  f.object.build_profile
-  f.inputs "User Details" do
+  f.object.profile ? f.object.profile : f.object.build_profile
+  if f.object.new_record?
+    f.inputs "User Details" do
       f.input :full_name
       f.input :email
       f.input :user_name
       f.input :is_disabled
-  end
-  f.inputs "Other Information", for: [:profile, f.object.profile] do |s|
-    s.input :personal_email
-    s.input :address1
-    s.input :address2
+    end
+    f.inputs "Other Information", for: [:profile, f.object.profile] do |s|
+      s.input :personal_email
+      s.input :address1
+      s.input :address2
+    end
+    else
+      f.inputs "User Details" do
+        f.input :full_name
+        f.input :email , :input_html => { :disabled => true }
+        f.input :user_name
+        f.input :is_disabled
+      end
+      f.inputs "Other Information", for: [:profile, f.object.profile] do |s|
+        s.input :personal_email
+        s.input :address1
+        s.input :address2
+    end
   end
   f.actions
 end
+
+
 
 controller do
   def create
