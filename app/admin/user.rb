7ae69136c8f03182690408987556e10e218ee0e5
selvavalli <selvavalli@railsfactory.org>
2014-05-27 19:47:36 +0530

    admin user change in activeadmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -66,13 +66,6 @@ controller do
       params.require(:user).permit(:full_name, :email, :user_name, :is_disabled, :company_id, profile_attributes: [:personal_email, :address2, :address1])
     end
 
-<<<<<<< HEAD
-    
-=======
-    def scoped_collection
-      current_company.users
-    end
->>>>>>> 14ade6c2d754d21d715ba284c85ca0ac69e45173
 end
 
 
