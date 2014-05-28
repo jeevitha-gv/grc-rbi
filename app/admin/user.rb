f3ca8c47fd5a25cb5393ed21ebe808abdd105a65
Madhusudhan <madhusudhan@railsfactory.org>
2014-05-28 14:14:53 +0530

    bug fixes for header image and added link to admin in the dropdown

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,32 +1,50 @@
 ActiveAdmin.register User do
+  menu :if => proc{ !current_admin_user.present? }
 
-
-menu :if => proc{ !current_admin_user.present? }
-
-
-# removing delete option
-actions :all, :except => [:destroy]
-
-
-index do
-  selectable_column
-  column :full_name
-  column :email
-  column :user_name
-  column :team
-  column :is_disabled
-  actions
-end
-
-show do
-  attributes_table :full_name, :email, :user_name
-end
+  # removing delete option
+  actions :all, :except => [:destroy]
 
   permit_params :full_name, :email, :user_name, :password , :password_confirmation, :is_disabled, profile_attributes: [:personal_email, :address2, :address1]
 
+  controller do
+    before_filter :check_company_admin
+    def create
+      @user = User.new(user_params)
+      @user.company_id = current_user.company_id
+      if @user.save
+        redirect_to admin_users_path
+      else
+        redirect_to new_admin_user_path
+      end
+    end
 
-form do |f|
-  f.object.profile ? f.object.profile : f.object.build_profile
+    private
+      def user_params
+        params.require(:user).permit(:full_name, :email, :user_name, :is_disabled, :company_id, profile_attributes: [:personal_email, :address2, :address1])
+      end
+
+      def scoped_collection
+        current_company.users
+      end
+  end
+
+  index do
+    selectable_column
+    column :full_name
+    column :email
+    column :user_name
+    column :team
+    column :is_disabled
+    actions
+  end
+
+  show do
+    attributes_table :full_name, :email, :user_name
+  end
+
+
+  form do |f|
+    f.object.profile ? f.object.profile : f.object.build_profile
     f.inputs "User Details" do
       f.input :full_name
       if f.object.new_record?
@@ -43,34 +61,6 @@ form do |f|
       s.input :address1
       s.input :address2
     end
-  f.actions
-end
-
-controller do
-   def scoped_collection
-      current_company.users
-    end
-  def create
-    @user = User.new(user_params)
-    @user.company_id = current_user.company_id
-    if @user.save
-      redirect_to admin_users_path
-    else
-      redirect_to new_admin_user_path
-    end
+    f.actions
   end
-
- 
-  private
-    def user_params
-      params.require(:user).permit(:full_name, :email, :user_name, :is_disabled, :company_id, profile_attributes: [:personal_email, :address2, :address1])
-    end
-
 end
-
-
-end
-
-
-
-
