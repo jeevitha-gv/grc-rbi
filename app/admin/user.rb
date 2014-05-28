4819090627900c9fef1c82b87c7b32f87d80c333
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-28 20:15:31 +0530

    company validation

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -7,13 +7,9 @@ ActiveAdmin.register User do
   permit_params :full_name, :email, :user_name, :password , :password_confirmation, :is_disabled, profile_attributes: [:personal_email, :address2, :address1]
 
   controller do
-<<<<<<< HEAD
-    #before_filter :check_company_admin
-  end
 
 # removing delete option
 actions :all, :except => [:destroy]
-=======
     before_filter :check_company_admin
     def create
       @user = User.new(user_params)
@@ -21,10 +17,10 @@ actions :all, :except => [:destroy]
       if @user.save
         redirect_to admin_users_path
       else
-        redirect_to new_admin_user_path
+        # redirect_to new_admin_user_path
+        render 'new'
       end
     end
->>>>>>> 452069ffd4383d87da5f3853c16e3eb5431bc962
 
     private
       def user_params
@@ -70,22 +66,6 @@ actions :all, :except => [:destroy]
       s.input :address1
       s.input :address2
     end
-<<<<<<< HEAD
-  f.actions
-end
-
-controller do
-  def create
-    @user = User.new(user_params)
-    @user.company_id = current_user.company_id
-    if @user.save
-      redirect_to admin_users_path
-    else
-      # redirect_to new_admin_user_path
-      render 'new'
-    end
-=======
     f.actions
->>>>>>> 452069ffd4383d87da5f3853c16e3eb5431bc962
-  end
+   end
 end
