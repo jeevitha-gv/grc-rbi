7eb4ac89576972a8f4037f0061e18d50778ed37a
selvavalli <selvavalli@railsfactory.org>
2014-05-29 18:02:54 +0530

    client & topic edit fixes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,8 @@
 class Topic < ActiveRecord::Base
     #publicactivity gem
-   include PublicActivity::Model
-   tracked owner: ->(controller, model) { controller && controller.current_user }
-   tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
+   #~ include PublicActivity::Model
+   #~ tracked owner: ->(controller, model) { controller && controller.current_user }
+   #~ tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
 
 validates :name, uniqueness:true
 validates :name, presence:true
