6c688000d0f3d60328035951d70eae9465d0bee8
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-29 17:09:04 +0530

    modular and profile validations added

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :country
   validates_numericality_of :phone_no, presence: true
-  validates :phone_no, length: { in: 7..30}
-  validates :address1, length: { in: 7..40 }
-  validates :address2, length: { in: 7..40 }
+  validates :phone_no, length: { in: 7..12}
+  validates :address1, length: { in: 7..40 }, :if => Proc.new{|f| !f.address1.blank? }
+  validates :address2, length: { in: 7..40 }, :if => Proc.new{|f| !f.address2.blank? }
 end
