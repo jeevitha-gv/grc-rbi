3d59b21e69e41288ed2358cc756985a577f76544
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-29 17:18:01 +0530

    priority and section model validations

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,5 +1,6 @@
 class Priority < ActiveRecord::Base
-  validates_format_of :name, :with =>/\A[a-zA-Z1-9]+\z/
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  
+  validates :name, presence: true, :if => Proc.new{ |f| f.name.blank? } 
+  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? } 
+  validates :name, uniqueness: true, :if => Proc.new{ |f| !f.name.blank? } 
 end
