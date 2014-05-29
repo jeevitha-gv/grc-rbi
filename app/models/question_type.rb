282973319a11bbec9669384100f29969d8ad5a88
selvavalli <selvavalli@railsfactory.org>
2014-05-29 15:33:42 +0530

    admin user authentication

--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -1,8 +1,8 @@
 class QuestionType < ActiveRecord::Base
   #publicactivity gem
-    include PublicActivity::Model
-    tracked owner: ->(controller, model) { controller && controller.current_user }
-    tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
+    #~ include PublicActivity::Model
+    #~ tracked owner: ->(controller, model) { controller && controller.current_user }
+    #~ tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
 
   validates_format_of :name, :with =>/\A[a-zA-Z1-9]+\z/
   validates :name, presence:true
