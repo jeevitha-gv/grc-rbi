fdc6d003afa09c332e6fabd04c78d72206224947
ghayathri <aghayathri@hotmail.com>
2014-05-29 15:37:45 +0530

    Validation for Team

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,7 +5,7 @@ has_many :users, :through => :user_teams
 has_many :user_teams
 
  validates :name, presence:true, :if => Proc.new{|f| f.name.blank? }
- validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d]+\Z/i, :if => Proc.new{ |f| !f.name.blank? } 
+ validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? } 
  validates :name, uniqueness:true, :if => Proc.new{ |f| !f.name.blank? } 
 
 end
