5e0a29c8c94f2221532f3e5a04feddf5cfcf80e2
Madhusudhan <madhusudhan@railsfactory.org>
2014-05-30 17:08:38 +0530

    added association and validations for audits

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,17 +1,20 @@
 
 class Client < ActiveRecord::Base
-
+  # associations
+  has_many :audits
   belongs_to :company #client belongs to a company
-  validates :name, presence:true, :if => Proc.new{|f| f.name.blank? } 
-  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? } 
-  validates :name, uniqueness:true, :if => Proc.new{ |f| !f.name.blank? } 
-  validates :name, length: { in: 4..52 }, :if => Proc.new{ |f| !f.name.blank? } 
-  validates :address1, length: { in: 7..40 },:if => Proc.new{|f| !f.address1.blank? } 
-  validates :address2, length: { in: 7..40 },:if => Proc.new{|f| !f.address2.blank? } 
-  validates :email, presence: true,:if => Proc.new{|f| f.email.blank? } 
-  validates :email, uniqueness: true,:if => Proc.new{|f| !f.email.blank? } 
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create,:if => Proc.new{|f| !f.email.blank? } 
-  validates :contact_no, presence:true, :if => Proc.new{|f| f.contact_no.blank? } 
-  validates_numericality_of :contact_no, :if => Proc.new{|f| !f.contact_no.blank? } 
-  validates :contact_no, length: { is: 10 }, :if => Proc.new{|f| !f.contact_no.blank? } 
+
+  # validations
+  validates :name, presence:true
+  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? }
+  validates :name, uniqueness:true, :if => Proc.new{ |f| !f.name.blank? }
+  validates :name, length: { in: 4..52 }, :if => Proc.new{ |f| !f.name.blank? }
+  validates :address1, length: { in: 7..40 },:if => Proc.new{|f| !f.address1.blank? }
+  validates :address2, length: { in: 7..40 },:if => Proc.new{|f| !f.address2.blank? }
+  validates :email, presence: true
+  validates :email, uniqueness: true,:if => Proc.new{|f| !f.email.blank? }
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create,:if => Proc.new{|f| !f.email.blank? }
+  validates :contact_no, presence:true
+  validates_numericality_of :contact_no, :if => Proc.new{|f| !f.contact_no.blank? }
+  validates :contact_no, length: { is: 10 }, :if => Proc.new{|f| !f.contact_no.blank? }
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,7 +1,10 @@
 class Department < ActiveRecord::Base
+  # associations
+  has_many :audits
   belongs_to :location
 
-  validates :name, presence:true, :if => Proc.new{|f| f.name.blank? } 
-  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i,:if => Proc.new{|f| !f.name.blank? } 
+  # validations
+  validates :name, presence:true
+  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i,:if => Proc.new{|f| !f.name.blank? }
   validates :location_id, presence:true
 end
