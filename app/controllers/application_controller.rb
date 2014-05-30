69810faacfc52c8966e8a744db411b0d6a231354
selvavalli <selvavalli@railsfactory.org>
2014-05-30 18:57:58 +0530

    rescue fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
   helper_method :current_company
   before_filter :check_subdomain
   before_filter :check_password_authenticated, :if => :current_user
-  #   if Rails.application.config.consider_all_requests_local
-  #   rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-  #   rescue_from ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
-  # end
+    unless Rails.application.config.consider_all_requests_local
+      rescue_from Exception, with: lambda { |exception| render_error 500, exception }
+      rescue_from ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
+    end
 
   protected
 
