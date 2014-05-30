36fbe29c40d819e46f7df1940ce3e297efba2978
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-05-30 11:47:14 +0530

    NC Questions Relationships done(except for Audit Model)

--- app/models/question_type.rb
+++ app/models/question_type.rb
@@ -3,6 +3,7 @@ class QuestionType < ActiveRecord::Base
     #~ include PublicActivity::Model
     #~ tracked owner: ->(controller, model) { controller && controller.current_user }
     #~ tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
+  has_many :nc_questions
   validates :name, presence:true, :if => Proc.new{ |f| f.name.blank? } 
   validates_format_of :name, :with =>/\A[a-zA-Z\s]+\z/, :if => Proc.new{ |f| !f.name.blank? } 
   validates :name, uniqueness:true, :if => Proc.new{ |f| !f.name.blank? }
