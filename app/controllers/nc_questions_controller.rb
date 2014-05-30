030a9c9631c8cffb0519c145e0c1437dc2bbe3e4
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-05-30 18:38:27 +0530

    Question getting saved, corrected association rule

--- app/controllers/nc_questions_controller.rb
+++ app/controllers/nc_questions_controller.rb
@@ -9,6 +9,7 @@ class NcQuestionsController < ApplicationController
 	end
 
 	def create
+		binding.pry
 		@nc_question = NcQuestion.new(question_params)
 		if @nc_question.save
 			redirect_to nc_questions_path
@@ -18,6 +19,7 @@ class NcQuestionsController < ApplicationController
 	end
 
 	def question_params
+		
 		params.require(:nc_question).permit(:question, :question_type_id, :priority_id, :target_date, :does_require_document, :nc_library, :auditee_id)
 	end
 
