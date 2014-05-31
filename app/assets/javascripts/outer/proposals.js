8ca10f7e7a988c5ca1cbaaafd03e82a63f32e4f8
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-05-31 16:54:56 +0530

    design integration for recommendation page

---
+++ app/assets/javascripts/outer/proposals.js
@@ -0,0 +1,16 @@
+var proposals = [{
+    ProductID : 1,
+    Name : "Smithfield Home - Spring 2012",
+    Type : "Split Gas/Electric",
+    Created : "20/20/2013"
+}, {
+    ProductID : 2,
+    Name : "Lennox 5 ton 80% Upto",
+    Type : "Split Gas/Electric ",
+    Created : "20/20/2013"
+}, {
+    ProductID : 3,
+    Name : "Santa",
+    Type : "Split Gas/Electric ",
+    Created : "20/20/2013"
+}];
---
+++ app/assets/javascripts/outer/salenotes.js
@@ -0,0 +1,13 @@
+var salenotes = [{
+    ProductID : 1,
+    Domain : "A6.11 Management Commitment to information Security",
+    Checklist : "Artifact Name",
+	priority:"test",
+	owner : "test",
+	respond : "test",
+	targetdate : " test"
+},{
+    ProductID : 11,
+    Domain : "A6.12 Information Security Co-ordination",
+    Checklist : "Artifact Name"
+}];
