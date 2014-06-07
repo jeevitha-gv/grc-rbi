84655313ad4ffbb180f654f271f2d09cfc3bd9a0
selvavalli <selvavalli@railsfactory.org>
2014-06-07 12:03:59 +0530

    checklist recommendations create for audit compliance

--- app/models/checklist_recommendation.rb
+++ app/models/checklist_recommendation.rb
@@ -10,5 +10,22 @@ class ChecklistRecommendation < ActiveRecord::Base
 	belongs_to :dependent_recommendation, :class_name => "ChecklistRecommendation"
 	belongs_to :blocking_recommendation, :class_name => "ChecklistRecommendation"
 	belongs_to :checklist, polymorphic: true
-
+	
+	#validation
+   validates :recommendation, presence: true
+   validates :reason, presence: true
+   validates :response_priority_id, presence: true
+   validates :response_priority_id, presence: true
+   validates :response_severity_id, presence: true
+   validates :closure_date, presence: true
+   validates :recommendation_status_id, presence: true
+	
+	def audit_checklist(checklist_input)
+		checklist_params =[]
+			checklist_recommendation_id = checklist_input[:checklist_recommendation].collect {|k,v| v}
+			checklist_recommendation_id.each do |record|
+			checklist_params << record
+		end
+		return checklist_params
+	end
 end
