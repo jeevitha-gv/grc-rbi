ca1b5d30939aa01192d7135745595ad00f55108b
Madhusudhan <madhusudhan@railsfactory.org>
2014-06-07 15:21:09 +0530

    added complaince library csv upload functionality in super admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   post 'admin/privileges/modal_previlege'
   post 'admin/compliance_libraries/compliance_control_objectives'
   post 'admin/compliance_libraries/compliance_controls'
   post 'admin/compliance_libraries/compliance_domains'
+  post 'admin/compliance_libraries/import_files'
+
   devise_for :users
 
   resources :home
@@ -27,7 +28,6 @@ Rails.application.routes.draw do
     post 'import_files', on: :collection
   end
 
-
   resource :user do
     collection do
       patch 'update_password'
@@ -42,9 +42,6 @@ Rails.application.routes.draw do
     get '/' => 'audits#index'
   end
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
-
   resources :companies do
     member do
       get 'settings'
@@ -52,65 +49,16 @@ Rails.application.routes.draw do
     collection do
       get 'welcome', to: 'companies#welcome', :as => :welcome
     end
-   end
+  end
 
-   # delete '/activities/clear_audit' => 'activities#clear_audit', :as => :clear_audit
-   resources :activities, :except => [:show]
+  # delete '/activities/clear_audit' => 'activities#clear_audit', :as => :clear_audit
+  resources :activities, :except => [:show]
 
-    resources :compliance_libraries
-    resources :audit_compliances
+  resources :compliance_libraries
+  resources :audit_compliances
 
+  get 'welcome', to: 'companies#welcome', :as => :welcome
 
-   get 'welcome', to: 'companies#welcome', :as => :welcome
-
-   # resources :privileges
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # You can have the root of your site routed with "root"
+  root 'home#index'
 end
