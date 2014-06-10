88328a7c52ca4808d014e26bf37eec34b57ac68e
selvavalli <selvavalli@railsfactory.org>
2014-06-10 16:10:49 +0530

    dashboard files

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,13 +1,11 @@
 class DashboardController < ApplicationController
-<<<<<<< HEAD
 	
 	def calender
 		@audit =Audit.where('id=?', 4)
 	end
-=======
+	
   def index  	
   	@audit_status = Audit.joins( :audit_status).select("name as status,count(audit_status_id) as count").group(:name)
   	@audit_locations = Audit.joins( :location).select("name ,count(location_id) as count").group(:name)
   end
->>>>>>> 83da2532104d607d0f859d6e7b2e457acbab75b3
 end
