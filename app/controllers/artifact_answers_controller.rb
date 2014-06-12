7297c8e82bee9576e5c0502cb7191c13b14f2b1d
sathish <sathish.subramanian@railsfactory.org>
2014-06-12 21:46:36 +0530

    Changes for response

---
+++ app/controllers/artifact_answers_controller.rb
@@ -0,0 +1,42 @@
+class ArtifactAnswersController < ApplicationController
+  
+  
+  def list_attachments
+    @artifact_answer = ArtifactAnswer.find(params[:id])
+    @attachments = @artifact_answer.attachments
+   render layout: false
+  end
+ 
+  def create_attachment
+    @artifact_answer = ArtifactAnswer.find(params[:id])
+    if params[:file]
+      params[:file].each do |attachment|
+        @artifact_answer.attachments.create(attachment_file: attachment)
+      end
+      
+    end
+    @attachments = @artifact_answer.attachments
+  end
+ 
+  def remove_attachment
+    attachment = Attachment.find(params[:id])
+    attachment.delete
+  end
+  
+  def list_comment
+    @artifact_answer = ArtifactAnswer.find(params[:id])
+    render layout: false
+  end
+  
+  def update_comment
+    @artifact_ans = ArtifactAnswer.find(params[:artifact_answer][:id])
+    @artifact_ans.update_attributes(comment_params)
+    @artifact_answer = ArtifactAnswer.find(params[:artifact_answer][:id])
+  end
+  
+  private
+
+  def comment_params
+    params.require(:artifact_answer).permit(:id, comment_attributes: [:comment, :id])
+  end
+end
--- app/models/artifact_answer.rb
+++ app/models/artifact_answer.rb
@@ -5,6 +5,13 @@ class ArtifactAnswer < ActiveRecord::Base
  belongs_to :artifact
  belongs_to :responsibility , :class_name => "User", foreign_key: 'responsibility_id'
  belongs_to :priority , :class_name => "Priority"
+ has_one :comment , as: :commentable
+ has_many :attachments, as: :attachable
+ 
+ accepts_nested_attributes_for :comment, reject_if: lambda { |a| a[:comment].blank? }, allow_destroy: true
 
+  delegate :name, to: :priority, prefix: true, allow_nil: true
+  delegate :full_name, to: :responsibility, prefix: true, allow_nil: true
+  delegate :name, to: :artifact, prefix: true, allow_nil: true
 
 end
