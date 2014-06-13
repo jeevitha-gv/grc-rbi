76e54163e1e4f071b7b8892d428e0f503d41c539
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-06-13 10:35:54 +0530

    #9938 - NC Answers Update IV

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,23 @@
 class AnswersController < ApplicationController
   def new
     @audit = Audit.find_by_id(params[:id])
-    @answers = Answer.new
-    @questions = @audit.nc_questions
   end
+
   def create
+
+    @audit = Audit.find(params[:audit][:audit_id])
+    if @audit.update_attributes(answer_params)
+      redirect_to answers_path
+    else
+      render "new"
+    end
   end
+
+  private
+  def answer_params
+    binding.pry
+    # params.require(:audit).permit(answer_attributes: [:value])
+  end
+
+
 end
