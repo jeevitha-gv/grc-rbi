15f31216edfb350aed2c48df98dbfee4c1b49ca3
sathish <sathish.subramanian@railsfactory.org>
2014-06-14 19:24:23 +0530

    Audit compliance

--- app/models/audit_compliance.rb
+++ app/models/audit_compliance.rb
@@ -3,6 +3,7 @@ class AuditCompliance < ActiveRecord::Base
 
 # Relationship
 	has_many :artifact_answers
+	has_many :artifacts, through: :artifact_answers
 	belongs_to :compliance_library
 	belongs_to :audit
 	belongs_to :score
