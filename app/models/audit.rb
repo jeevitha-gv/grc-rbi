3c046e1ba7aaa65c3dac2a6188b88aee6043fe69
Madhusudhan <madhusudhan@railsfactory.org>
2014-06-16 14:02:19 +0530

    Validations for Audit Creation - H

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -40,13 +40,13 @@ class Audit < ActiveRecord::Base
   validates :compliance_type, presence:true
   validates :standard_id, presence:true, :if => Proc.new{ |f| !f.compliance_type.blank? }
   validates_format_of :issue, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.issue.blank? }
-  validates :scope, length: { in: 4..250 }, :if => Proc.new{ |f| !f.scope.blank? }
-  validates :context, length: { in: 4..250 }, :if => Proc.new{ |f| !f.context.blank? }
-  validates :methodology, length: { in: 4..250 }, :if => Proc.new{ |f| !f.methodology.blank? }
-  validates :deliverables, length: { in: 4..250 }, :if => Proc.new{ |f| !f.deliverables.blank? }
-  validates :objective, length: { in: 4..250 }, :if => Proc.new{ |f| !f.objective.blank? }
-  validates :close_reason, length: { in: 4..250 }, :if => Proc.new{ |f| !f.close_reason.blank? }
-  validates :observation, length: { in: 4..250 }, :if => Proc.new{ |f| !f.observation.blank? }
+  validates :scope, length: { in: 0..250 }, :if => Proc.new{ |f| !f.scope.blank? }
+  validates :context, length: { in: 0..250 }, :if => Proc.new{ |f| !f.context.blank? }
+  validates :methodology, length: { in: 0..250 }, :if => Proc.new{ |f| !f.methodology.blank? }
+  validates :deliverables, length: { in: 0..250 }, :if => Proc.new{ |f| !f.deliverables.blank? }
+  validates :objective, length: { in: 0..250 }, :if => Proc.new{ |f| !f.objective.blank? }
+  validates :close_reason, length: { in: 0..250 }, :if => Proc.new{ |f| !f.close_reason.blank? }
+  validates :observation, length: { in: 0..250 }, :if => Proc.new{ |f| !f.observation.blank? }
   validates :start_date, presence: true
   validates :end_date, presence: true
   validate :check_auditees_uniq
