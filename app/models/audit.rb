b9b1fc17f54c63353c92d3a8e3d2dbb4f88dc943
sathish <sathish.subramanian@railsfactory.org>
2014-06-17 22:15:59 +0530

    Audit and status

--- app/models/audit.rb
+++ app/models/audit.rb
@@ -138,7 +138,7 @@ class Audit < ActiveRecord::Base
       operational_area = OperationalArea.find_or_initialize_by(compliance_library_id: k, company_id: company.id)
       if(operational_area.new_record?)
         operational_area.weightage = 1
-        operational_area.company_id = current_company.id
+        operational_area.company_id = company.id
         operational_area.save
       end
 
@@ -246,4 +246,34 @@ class Audit < ActiveRecord::Base
   def check_for_auditor_in_auditees
     self.errors[:auditees] = MESSAGES['audit']['failure']['auditor_not_in_auditees'] if audit_auditees.map(&:user_id).include?(self.auditor)
   end
+  
+  def build_audit_compliance(compliance_params)
+      old_compliance = self.audit_compliances.map(&:id)
+      compliance_params.each do |k, v|
+        audit_compliance = AuditCompliance.find_or_create_by(compliance_library_id: v["compliance_library_id"], audit_id: self.id)
+        old_compliance.delete(audit_compliance.id)
+        old_artifact_answers = audit_compliance.artifact_answers.map(&:id)
+        if(v["artifact_id"].present?)
+            artifact_ids =  v["artifact_id"].class == Array ? v["artifact_id"] : v["artifact_id"].split(",")
+            artifact_ids.each do |artifact_id|
+              artifact_answer = ArtifactAnswer.find_or_initialize_by(artifact_id: artifact_id.to_i, audit_compliance_id: audit_compliance.id)
+              old_artifact_answers.delete(artifact_answer.id)
+              artifact_answer.update(v.reject{|x| x=="artifact_id" || x=="compliance_library_id" || x=="artifact_answers"})
+            end
+        else
+            if(v["artifact_answers"].present?)
+              artifact_answer = ArtifactAnswer.find(v["artifact_answers"].to_i) rescue ""
+              if(artifact_answer.present? && !artifact_answer.artifact_id.present?)
+                old_artifact_answers.delete(artifact_answer.id)
+                artifact_answer.update(v.reject{|x| x=="artifact_id" || x=="compliance_library_id" || x == "artifact_answers"})
+              end
+            else
+              artifact_answer = audit_compliance.artifact_answers.create(v.reject{|x| x=="artifact_id" || x=="compliance_library_id" || x == "artifact_answers"})
+            end
+        end      
+        ArtifactAnswer.delete(old_artifact_answers) if old_artifact_answers.present?
+      end
+      AuditCompliance.delete(old_compliance) if old_compliance.present?
+    end
+    
 end
