1fa91dbb55f914734d5460eee66fb1f44ec920e9
selvavalli <selvavalli@railsfactory.org>
2014-06-19 14:23:33 +0530

    risk scoring associations

--- app/models/risk_scoring.rb
+++ app/models/risk_scoring.rb
@@ -1,11 +1,6 @@
 class RiskScoring < ActiveRecord::Base
-<<<<<<< HEAD
 	
-	belongs_to :scoring, :polymorphic => true
-
-=======
-
 	# Associations
 	has_many :control_measure
->>>>>>> 72019b43f343ff81b7f400c0c7331dc84a20c1ef
+	belongs_to :scoring, :polymorphic => true
 end
