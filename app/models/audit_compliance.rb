4be3401688edc0536b1f3e3d988411c6f9d1be0b
selvavalli <selvavalli@railsfactory.org>
2014-06-23 10:30:36 +0530

    audit compliance changes

--- app/models/audit_compliance.rb
+++ app/models/audit_compliance.rb
@@ -6,13 +6,9 @@ class AuditCompliance < ActiveRecord::Base
 	belongs_to :compliance_library
 	belongs_to :audit
 	belongs_to :score
-<<<<<<< HEAD
+
 	has_one :checklist_recommendation, as: :checklist , :dependent => :destroy
   
-=======
-	has_one :checklist_recommendation, as: :checklist
-
->>>>>>> f32fef50d498ab48f4511e2c5ed9cdb8904da41d
   delegate :name, to: :compliance_library, prefix: true, allow_nil: true
 
 end
