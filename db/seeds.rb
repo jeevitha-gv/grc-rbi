fc0392838be0e63ce1e62a39dafe6d74eb25b13e
ghayathri <aghayathri@hotmail.com>
2014-06-24 10:27:16 +0530

    Rails Best Practice

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+AdminUser.create!(email: 'admin@fixnix.com', password: 'adminfixnix', password_confirmation: 'adminfixnix')
+
 QuestionType.delete_all
 
     QuestionType.create([{name: 'Yes or No'}])
