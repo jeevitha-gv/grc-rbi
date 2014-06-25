d9db09d1feea4f4cc97243bbdd8ccc47f0b6ea91
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-06-25 20:44:40 +0530

    nc recommendations changes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,30 +9,38 @@ class Answer < ActiveRecord::Base
   delegate :question, :to => :nc_question, allow_nil: true
   delegate :name, :to => :question_type, prefix: true, allow_nil: true
 
+  # validates :value, presence: true, :if => Proc.new{|f|  f.nc_question.does_require_document == true }
+validate :presence_of_attachment
   # validates :detailed_value, presence:true
   # validates :value, presence:true
   # Build Answer form Nc Question selected
   def self.build_answer(params)
+    errors = {}
     params[:answer] && params[:answer].each do |nc_id, answer|
-        attachment = answer.delete("attachment")
+        # attachment = answer.delete("attachment")
         nc_que = NcQuestion.where(id: nc_id).last
         unless nc_que.answer.present?
             nc_que.answer = Answer.new(answer)
             if params[:commit] == "Submit Answers to Auditor"
               nc_que.is_answered = true
-              nc_que.answer.attachment = Attachment.new(attachment_file: attachment) if attachment.present?
+              nc_que.answer.attachment = Attachment.new(attachment_file: answer[:attachment]) if answer[:attachment].present?
               nc_que.save
             end
         else
-            nc_que.answer.attachment = nc_que.answer.attachment.nil? ? nc_que.answer.attachment = Attachment.new : nc_que.answer.attachment
-            nc_que.answer.attachment.update(attachment_file: attachment) if attachment.present?
             if nc_que.answer.update(answer)
               if params[:commit] == "Submit Answers to Auditor"
                 nc_que.is_answered = true
                 nc_que.save
               end
+              answer_attachment = nc_que.answer.attachment.nil? ? nc_que.answer.build_attachment : nc_que.answer.attachment
+              answer_attachment.update(attachment_file: answer[:attachment]) if answer[:attachment].present?
             end
         end
     end
   end
+
+  private
+    def presence_of_attachment
+      self.errors[:document_reuired] = MESSAGES['audit']['failure']['auditor_not_in_auditees'] if self.nc_question.does_require_document
+    end
 end
