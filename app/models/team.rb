419599295fa39c75c0fff97b5738da59aa0f54a0
selvavalli <selvavalli@railsfactory.org>
2014-06-26 15:55:30 +0530

    validation changes

--- app/models/team.rb
+++ app/models/team.rb
@@ -9,7 +9,7 @@ class Team < ActiveRecord::Base
 
   # validations
   validates :name, presence:true
-  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? }
+  validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s -]+\Z/i, :if => Proc.new{ |f| !f.name.blank? }
   validates :name, :uniqueness => {:scope => :company_id}, :if => Proc.new{ |f| !f.name.blank? }
 
   #scope
