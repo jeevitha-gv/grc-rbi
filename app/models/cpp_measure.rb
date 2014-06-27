a2178a09e3949ffd91c39621b1646eb12f15fe9f
selvavalli <selvavalli@railsfactory.org>
2014-06-27 14:03:42 +0530

    control, procedure & process functions

--- app/models/cpp_measure.rb
+++ app/models/cpp_measure.rb
@@ -4,4 +4,12 @@ class CppMeasure < ActiveRecord::Base
   belongs_to :company
   belongs_to :implementation_status
   belongs_to :compliance
+  
+  #Validations
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :compliance_id, presence: true
+  validates :company_id, presence: true
+
+  
 end
