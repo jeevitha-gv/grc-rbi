2c02c09b3169b7ee10c0d590bbb498cc80cf2318
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-06-30 19:16:19 +0530

    Categories per company and modified locale

--- db/migrate/20140618141554_create_risk_categories.rb
+++ db/migrate/20140618141554_create_risk_categories.rb
@@ -2,6 +2,7 @@ class CreateRiskCategories < ActiveRecord::Migration
   def change
     create_table :risk_categories do |t|
       t.string :name
+      t.integer :company_id
 
       t.timestamps
     end
