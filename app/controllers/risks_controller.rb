b8b916c376026f7cb574a4ef846eabbfb1250f6c
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-07-01 10:10:08 +0530

    Risk Creation Page

--- app/controllers/risks_controller.rb
+++ app/controllers/risks_controller.rb
@@ -11,6 +11,21 @@ class RisksController < ApplicationController
     @risk = current_company.risks.build(risk_params)
   end
 
+  def compliance_libraries
+    @compliance_libraries = ComplianceLibrary.for_id_and_leaf(params[:compliance_id])
+  end
+
+  def department_teams_users
+    risk_initializers(params[:location_id], params[:department_id], params[:team_id])
+  end
+
+  protected
+    def risk_initializers(location_id = nil, department_id = nil, team_id = nil)
+      @departments = Department.for_location(location_id) if location_id
+      @teams = Team.for_department_and_company(department_id, current_company.id) if department_id
+      @team = Team.for_id(team_id).last if team_id
+    end
+
   private
 
   def risk_params
