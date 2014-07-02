3980cb2a75ffa778b28767ac5b6f004a85b0ae8e
Poongothai <poongothai.l@railsfactory.org>
2014-07-02 10:54:00 +0530

    files added for subscription mails

---
+++ app/mailers/subscription_notifier.rb
@@ -0,0 +1,19 @@
+class SubscriptionNotifier < ActionMailer::Base
+  default from: "noreply@fixrnix.in"
+
+  def payment_complete(user_email)
+    mail(to: user_email.join(","), subject: 'Subscription Payment Successful',content_type: "text/html")
+  end
+
+  def payment_failed(user_email)
+    mail(to: user_email.join(","), subject: 'Subscription Payment Failed.',content_type: "text/html")
+  end
+
+  def plan_expire_notification(company)
+    mail(to: company.join(","), subject: 'Subscription expires today',content_type: "text/html")
+  end
+
+  def plan_notification(company)
+     mail(to: company.join(","), subject: 'Subscription will expire in 2 days',content_type: "text/html")
+  end
+end
