5db9fc87420682654b384840349963dff3e627cc
selvavalli <selvavalli@railsfactory.org>
2014-07-03 10:59:26 +0530

    merge changes

--- app/models/risk_category.rb
+++ app/models/risk_category.rb
@@ -1,11 +1,5 @@
 class RiskCategory < ActiveRecord::Base
 
-<<<<<<< HEAD
-  #Assosciations
-  belongs_to :company
-
-=======
   has_many :risks
   belongs_to :company
->>>>>>> 40322c09dcebf3daf1b7ccee7d494b3a061166f2
 end
