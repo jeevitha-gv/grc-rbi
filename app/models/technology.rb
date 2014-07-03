5a2541a9bc283ef3548ef05e9f5713102b4e2d8d
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-07-03 14:39:59 +0530

    Risk & Technology scopes added and OWASP file modified

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -3,4 +3,8 @@ class Technology < ActiveRecord::Base
   # Associations
   has_many :risks
   belongs_to :company
+
+  # Scopes
+  scope :for_company, lambda {|id| where(:company_id => [id, nil] ) }
+
 end
