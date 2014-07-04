9ef418c9e50a3883d86171362a0a15c594aec885
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-07-04 21:00:54 +0530

    mitigation multiselect

--- app/models/control_measure.rb
+++ app/models/control_measure.rb
@@ -3,4 +3,13 @@ class ControlMeasure < ActiveRecord::Base
 	# Associations
 	belongs_to :risk
 	belongs_to :risk_scoring
+	
+	before_save :check_empty_params
+	before_update :check_empty_params
+	
+	def check_empty_params
+		self.control_ids = self.control_ids.reject{|x| !x.present?}
+		self.process_ids = self.process_ids.reject{|x| !x.present?}
+		self.procedure_ids = self.procedure_ids.reject{|x| !x.present?}
+	end
 end
