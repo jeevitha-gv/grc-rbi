b80771fe62bb868d3785258328fb277c592f7a17
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-07-07 12:57:50 +0530

    Modified mail template data

--- app/models/mgmt_review.rb
+++ app/models/mgmt_review.rb
@@ -3,7 +3,7 @@ class MgmtReview < ActiveRecord::Base
 	# Associations
 	belongs_to :risk
 	belongs_to :review
-	belongs_to :reviewer, class_name: "User", foreign_key: :reviewer
+	belongs_to :mgmt_reviewer, class_name: "User", foreign_key: :reviewer
 	belongs_to :next_step
 	has_one :comment , as: :commentable, class_name: "Comment"
 	accepts_nested_attributes_for :comment, :allow_destroy => true
