5bb32fffbe6709f5d9c32c4dbd486bb02b0215dc
ghayathri <aghayathri@hotmail.com>
2014-07-11 18:48:43 +0530

    Menu is added to Privileges

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -1,7 +1,7 @@
 module ActiveAdmin::ViewsHelper #camelized file name
   def add_admin_active_class(params)
     case params[:controller]
-    when 'admin/settings', 'admin/departments', 'admin/locations', 'admin/teams', 'admin/users', 'admin/clients', 'admin/roles', 'admin/dashboard', 'admin/compliances', 'admin/companies', 'admin/modulars', 'admin/priorities', 'admin/question_types', 'admin/languages', 'admin/sections', 'admin/topics', 'admin/scores', 'admin/plans'
+    when 'admin/settings', 'admin/departments', 'admin/locations', 'admin/teams', 'admin/users', 'admin/clients', 'admin/roles', 'admin/dashboard', 'admin/compliances', 'admin/companies', 'admin/modulars', 'admin/priorities', 'admin/question_types', 'admin/languages', 'admin/sections', 'admin/topics', 'admin/scores', 'admin/plans', 'admin/privileges'
       'Settings'
     when 'admin/operational_areas','admin/artifacts','admin/reminders'
       'Audit'
@@ -26,7 +26,7 @@ module ActiveAdmin::ViewsHelper #camelized file name
   end
 
   def add_sub_menu(action_path)
-    if ["clients","locations", "departments", "teams","roles","users","settings", "plans"].include?(action_path)
+    if ["clients","locations", "departments", "teams","roles" ,"privileges","users","settings", "plans"].include?(action_path)
       'overview'
     elsif ["operational_areas","artifacts", "reminders"].include?(action_path)
       'audit'
