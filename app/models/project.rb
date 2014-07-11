72f27d6f2ea8e8d6ad4bae57556d1d8fbfa5cb8a
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-07-11 14:59:09 +0530

    project validation added with scope

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,9 @@ class Project < ActiveRecord::Base
 	# Associations
   has_many :risks
 	belongs_to :company
+
+   validates :name, presence:true
+   validates :name, :uniqueness => {:scope => :company_id}, :if => Proc.new{ |f| !f.name.blank? }
+
+   scope :for_name_by_company, lambda {|title, company_id| where("name = ? AND company_id = ?", name, company_id)}
 end
