cb0e5cabe64da7417a0f4a2aad6ef7d61d2dc70d
selvavalli <selvavalli@railsfactory.org>
2014-07-14 10:20:39 +0530

    main board chart sortable function

--- app/models/dashboard_chart.rb
+++ app/models/dashboard_chart.rb
@@ -1,11 +1,15 @@
 class DashboardChart < ActiveRecord::Base
 	
+	validates :name, presence:true
+	validates :x_axis, presence:true
+	validates :y_axis, presence:true
+	validates :chart_type, presence:true
+	
 	#Association
 	belongs_to :company
 	
 	def self.update_chart_order(order)
-		chart_order = order.split('|')
-		chart_order.each_with_index do |id, index|
+		order.each_with_index do |id, index|
 			dashboard_chart = DashboardChart.find_by_id(id)
 			dashboard_chart.update(:order => index)
 		end
