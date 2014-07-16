ca2b20018de79e0866cf1e7781f820bcc2503487
padmapriyasrinivasan <padmapriyasrini@outlook.com>
2014-07-16 19:24:14 +0530

    Validation fixes in client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,9 +1,10 @@
 
 class Client < ActiveRecord::Base
-  #publicactivity gem
-  include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller && controller.current_user }
-  tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
+
+ #publicactivity gem
+   include PublicActivity::Model
+   tracked owner: ->(controller, model) { controller && controller.current_user }
+   tracked ip: ->(controller,model) {controller && controller.current_user.current_sign_in_ip}
 
   # associations
   has_many :audits
@@ -13,12 +14,12 @@ class Client < ActiveRecord::Base
   # validations
   validates :name, presence:true
   validates_format_of :name, :with =>/\A(?=.*[a-z])[a-z\d\s]+\Z/i, :if => Proc.new{ |f| !f.name.blank? }
-  validates_uniqueness_of :name, :case_sensitive => false, :if => Proc.new{ |f| !f.name.blank? }
+  validates_uniqueness_of :name, scope: [:company_id], :case_sensitive => false, :if => Proc.new{ |f| !f.name.blank? }
   validates :name, length: { in: 2..52 }, :if => Proc.new{ |f| !f.name.blank? }
   validates :address1, length: { in: 2..40 },:if => Proc.new{|f| !f.address1.blank? }
   validates :address2, length: { in: 2..40 },:if => Proc.new{|f| !f.address2.blank? }
   validates :email, presence: true
-  validates :email, uniqueness: true,:if => Proc.new{|f| !f.email.blank? }
+  validates :email, uniqueness: {scope: :company_id},:if => Proc.new{|f| !f.email.blank? }
   validates :email, length: { maximum: 50 },:if => Proc.new{|f| !f.email.blank? }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,:if => Proc.new{|f| !f.email.blank? }
   validates :contact_no, presence:true, length: { in: 10..15 }
