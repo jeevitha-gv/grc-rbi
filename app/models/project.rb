d6f621ce3b467bb4cade726aaa831d021916be30
sunil <sunilkalyan08@gmail.com>
2014-07-24 12:02:31 +0530

    Projects order field negative values fixed

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,6 +7,6 @@ class Project < ActiveRecord::Base
    validates :name, presence:true
    validates :name, :uniqueness => {:scope => :company_id}, :if => Proc.new{ |f| !f.name.blank? }
    validates :name, length: { maximum: 50 }, :if => Proc.new{|f| !f.name.blank? }
-
+   validates :order, :numericality => { :greater_than_or_equal_to => 1, :less_than_or_equal_to => 20, :only_integer => true}
    scope :for_name_by_company, lambda {|title, company_id| where("name = ? AND company_id = ?", name, company_id)}
 end
