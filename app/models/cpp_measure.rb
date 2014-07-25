51c7f571c6aff97e8fbe4d66fc4b3c856e059e89
aghayathri <ghayathri11@gmail.com>
2014-07-25 12:46:21 +0530

    Validation for implementation status,duration in cpp measure

--- app/models/cpp_measure.rb
+++ app/models/cpp_measure.rb
@@ -13,6 +13,8 @@ class CppMeasure < ActiveRecord::Base
   validates :description, presence: true
   validates :compliance_id, presence: true
   validates :company_id, presence: true
+  validates :duration, presence: true
+  validates :implementation_status_id, presence: true
 
   scope :for_type_by_company, lambda {|measure_type, company_id| where("measure_type = ? AND company_id = ?", measure_type, company_id)}
 
