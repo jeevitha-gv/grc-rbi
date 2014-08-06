81c994fa3b1cb7726b49abe8baf53448e387008f
selvaganapathi <ganpathi1990@gmail.com>
2014-08-06 17:25:19 +0530

    selva - unwanted files removed frm incident management

--- db/migrate/20140804132947_create_escalations.rb
+++
@@ -1,8 +0,0 @@
-class CreateEscalations < ActiveRecord::Migration
-  def change
-    create_table :escalations do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140804133001_create_slas.rb
+++
@@ -1,8 +0,0 @@
-class CreateSlas < ActiveRecord::Migration
-  def change
-    create_table :slas do |t|
-
-      t.timestamps
-    end
-  end
-end
