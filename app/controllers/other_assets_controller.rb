20e3f9cb5249d7d6b87f0eb48104f33e5b065a2f
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-08-09 17:34:57 +0530

    Revert "Final changes added"
    This reverts commit 8db86875875d10921458f2169d7fa06cbe812c65.

--- app/controllers/other_assets_controller.rb
+++ app/controllers/other_assets_controller.rb
@@ -1,25 +1,5 @@
 class OtherAssetsController < ApplicationController
-
   def index
   	@other_assets = current_company.other_assets
   end
-
-  def new
-  	@other_asset = OtherAsset.new
-  end
-
- def create
-    @asset = current_company.other_assets.new(asset_params)
-    if @asset.save
-      redirect_to other_assets_path
-    else
-      render 'new'
-    end
-  end
-
-  private
-    def asset_params
-      params.require(:other_asset).permit(:company_id, :name, :manufacturer, :asset_type_id, :asset_status_id, :model, :serial_number, :asset_id, :ip, :description, :asset_owner, :asset_user, :location_id, :department_id, :maintenance_contact, :lease_contact)
-    end
-
 end
