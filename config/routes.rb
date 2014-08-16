8ed29537b0e7d062c42cc51fba2bf818b1748689
akilakiran <joinakila@gmail.com>
2014-08-16 11:32:46 +0530

    routes path added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-
-  
-
-
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   post 'admin/privileges/modal_previlege'
@@ -161,7 +156,7 @@ Rails.application.routes.draw do
 end
 end
 
-
+root 'home#index'
 
 
 end
