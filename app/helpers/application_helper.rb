5e0cbdae807eff1c603a52f9324e19286de28944
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-08-19 11:59:27 +0530

    Lifecycle fields done basic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -84,4 +84,13 @@ end
   def company_plan_check
     current_company.plan.expires.to_date < Date.today if current_company.plan.expires.present?
   end
+
+def link_to_add_lifecycle_fields(name, f, association, lifecycle)
+  new_object = f.object.class.reflect_on_association(association).klass.new
+  fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+    render(association.to_s.singularize + "_fields", :f => builder, lifecycle: lifecycle)
+  end
+  link_to "#{name}" ,'javascript:void(0)',  {onclick: "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: "plus-icon"}
+end
+
 end
