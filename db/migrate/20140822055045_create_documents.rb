9cbaf233cef843412a1930c691c82b834ee3980d
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-09-17 10:37:02 +0530

    Modified Documents Model as Child Model

--- db/migrate/20140822055045_create_documents.rb
+++ db/migrate/20140822055045_create_documents.rb
@@ -1,25 +1,14 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
     create_table :documents do |t|
-      t.string :name
-      t.text :description
-      t.integer :impact_id
       t.integer :document_status_id
       t.integer :document_type_id
       t.float :version
-      t.integer :location_id
-      t.integer :department_id
-      t.integer :asset_manager_id
-      t.integer :asset_user_id
       t.datetime :assigned_on
 
       t.timestamps
     end
     add_index :documents, :document_status_id
-    add_index :documents, :document_type_id
-    add_index :documents, :location_id
-    add_index :documents, :department_id
-    add_index :documents, :asset_manager_id
-    add_index :documents, :asset_user_id
+    add_index :documents, :document_type_id    
   end
 end
