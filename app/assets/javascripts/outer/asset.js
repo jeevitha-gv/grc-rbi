3e05ea71de9de1c437a1ec824f182c597a704e7b
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-09-19 18:51:27 +0530

    Score for Edit Operation also

--- app/assets/javascripts/outer/asset.js
+++ app/assets/javascripts/outer/asset.js
@@ -7,12 +7,26 @@ $( document ).ready(function() {
     var total_score = conf + inte + avai;
     $('#total_score').text(total_score);
     if( total_score > 7 && total_score <= 9 )
-      $('#score_color').removeClass().addClass('high-box');         
+      $('#score_color').removeClass().addClass('high-box');
     else if( total_score > 5 && total_score <= 7 )
       $('#score_color').removeClass().addClass('medium-box');
     else if( total_score >= 3 && total_score <= 5 )
       $('#score_color').removeClass().addClass('low-box');
   });
+});
+
+$( document ).ready(function() {
+    var conf = parseInt($("#conf").val());
+    var inte = parseInt($("#inte").val());
+    var avai = parseInt($("#avai").val());
+    var total_score = conf + inte + avai;
+    $('#total_score').text(total_score);
+    if( total_score > 7 && total_score <= 9 )
+      $('#score_color').removeClass().addClass('high-box');
+    else if( total_score > 5 && total_score <= 7 )
+      $('#score_color').removeClass().addClass('medium-box');
+    else if( total_score >= 3 && total_score <= 5 )
+      $('#score_color').removeClass().addClass('low-box');
 });
 
 // For Semantic Dropdown
