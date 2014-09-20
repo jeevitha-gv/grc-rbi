5d377581985730d3a7d4f9ef8c2247d64acd927a
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-09-20 11:08:19 +0530

    Flow coming up. Scope problem resolved

--- config/routes.rb
+++ config/routes.rb
@@ -87,14 +87,6 @@ Rails.application.routes.draw do
     resource :answers, only: [:index, :create, :new]
  end
 
-resources :computers do
-    collection do
-    post 'computer_imports'
-    get 'computer_export'
-  end
- end
-  
-
  resources :vendors do
   collection do
     post 'vendor_imports'
