0c8cb8a209b618f321ce7d2b0757c46fe8150c87
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2014-09-20 11:18:32 +0530

    BP to SourceCode Controller

--- app/controllers/source_codes_controller.rb
+++ app/controllers/source_codes_controller.rb
@@ -1,6 +1,9 @@
 class SourceCodesController < ApplicationController
+
+  layout 'asset_layout'
+
   def index
-  	  	@sourcecode = current_company.assets
+  	@sourcecode = current_company.assets
   end
 
   def new
@@ -9,16 +12,14 @@ class SourceCodesController < ApplicationController
   end
 
   def create
-
     @sourcecode = SourceCode.new(sourcecode_params)
     @sourcecode.asset.company_id = current_company.id
     @sourcecode.asset.identifier_id = current_user.id
     @sourcecode.asset.asset_state_id = 1
-
   	if @sourcecode.save
   		redirect_to source_codes_path
   	else
-  		redirect_to new_source_code_path
+  		render new
   	end
   end
 
@@ -26,7 +27,19 @@ class SourceCodesController < ApplicationController
   	@sourcecode = SourceCode.find(params[:id])
   end
 
-  def sourcecode_params
-  	params.require(:source_code).permit(:vcs_url, :project_manager, :version_number, :backup_measure, :assigned_on, :expected_life, asset_attributes: [:id,:name, :description, :location_id, :department_id,:asset_state_id,:classification_id,:company_id, :owner_id,:custodian_id,:identifier_id,:evaluated_by,:personal_data,:sensitive_date,:customer_data,:confidentiality,:availability,:integrity])
+  def update
+    @source_code = SourceCode.find(params[:id])
+    if @source_code.update_attributes(info_assets)
+      redirect_to source_codes_path
+    else
+      render new
+    end
   end
+
+  private
+
+  def sourcecode_params
+  	params.require(:source_code).permit(:vcs_url, :project_manager, :version_number, :backup_measure, :assigned_on, :expected_life, asset_attributes: [:id, :name, :description, :location_id, :department_id, :asset_state_id, :classification_id, :company_id, :owner_id, :custodian_id, :identifier_id, :evaluated_by, :personal_data, :sensitive_date, :customer_data, :confidentiality, :availability, :integrity])
+  end
+
 end
