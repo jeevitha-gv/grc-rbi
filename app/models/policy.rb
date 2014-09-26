233547c3b5df3f723505562b44d3888f35849e17
Chris Oliver <jomonsebin@gmail.com>
2014-09-26 12:39:04 +0530

    policy.rb

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -22,7 +22,32 @@ class Policy < ActiveRecord::Base
 	has_one  :publish_policy
 	has_many :policy_approvals
 	has_many :attachments, as: :attachable
+  validates :title,length: { in: 0..250 }, :if => Proc.new{ |f| !f.title.blank? }
+  validates_presence_of :title
+  validates :policy_kind_id, presence:true
+  validates :audience_id, presence:true
+  validates :policy_classification_id, presence:true
+  # validates_presence_of :location_ids
+  # validates_presence_of :department_ids
+  validates :scope, presence:true
+  validates :purpose, presence:true
 
-	accepts_nested_attributes_for :policy_locations, reject_if: lambda { |a| a[:user_id].blank? },:allow_destroy => true
+  validates :description, presence:true
+  validates :note, length: { in: 0..250 }
+  validates :standard_id, presence:true
+  validates :owner, presence:true
+  validates :effective_from, presence:true
+
+  validates :effective_till, presence:true
+  validates :expected_publish_date, presence:true
+  validates :review_within_date, presence:true
+  validates :policy_status_id, presence:true
+    
+    delegate :name, to: :policy_classification, prefix: true, allow_nil: true
+    delegate :name, to: :audience, prefix: true, allow_nil: true
+    delegate :name, to: :policy_kind, prefix: true, allow_nil: true
+    delegate :user_name, to: :policy_owner, prefix: true, allow_nil: true
+
+  accepts_nested_attributes_for :policy_locations, reject_if: lambda { |a| a[:user_id].blank? },:allow_destroy => true
 	
 end
